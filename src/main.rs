75b96eb7553dc0beeb343aa1ce9f7ad62defae9e
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2020-11-09 11:12:03 +0100

    Added rust files and dependences

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+use termion::raw::IntoRawMode;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Widget};
+use tui::Terminal;
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        f.render_widget(block, size);
+    })
+}
