c5ff931d79d1b90e0cb1c49273ae91c971b17a14
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2020-11-13 19:21:33 +0100

    Added input thread but deadlocks

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use reqwest;
+use std::collections::VecDeque;
 use std::io;
+use std::sync::{Arc, Mutex};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, Notify};
 use tui::backend::TermionBackend;
 use tui::widgets::{Block, Borders};
 use tui::Terminal;
@@ -13,35 +15,71 @@ async fn request_content(url: &str) -> reqwest::Result<String> {
     reqwest::get(url).await?.text().await
 }
 
-const INPUT: u8 = 10;
+async fn update_content(sources: Vec<String>) -> Option<Vec<String>> {
+    // Update only if there is a source to update from
+    if sources.len() > 0 {
+        let (sender, mut receiver) = mpsc::channel(sources.len());
+        let mut content_vec: Vec<String> = Vec::with_capacity(sources.len());
 
-async fn update_content(sources: Vec<String>) {
-    let (sender, mut receiver) = mpsc::channel(sources.len());
-    let mut content_vec: Vec<String> = Vec::with_capacity(sources.len());
+        for source in sources {
+            let mut sender = sender.clone();
+            tokio::spawn(async move {
+                let content = request_content(&source).await.unwrap();
+                sender.send(content).await.unwrap();
+            });
+        }
+        drop(sender);
 
-    for source in sources {
-        let mut sender = sender.clone();
-        tokio::spawn(async move {
-            let content = request_content(&source).await.unwrap();
-            sender.send(content).await.unwrap();
-        });
+        while let Some(content) = receiver.recv().await {
+            content_vec.push(content)
+        }
+        return Some(content_vec);
     }
-    drop(sender);
-
-    while let Some(content) = receiver.recv().await {
-        content_vec.push(content)
-    }
+    None
 }
 
-fn input_thread() {}
+fn input_thread(inputs: &Arc<Mutex<VecDeque<Key>>>, notify: &Arc<Notify>) {
+    let inputs = inputs.clone();
+    let notify = notify.clone();
+    tokio::spawn(async move {
+        let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(50));
+        let stdin = io::stdin();
+
+        loop {
+            println!("Test");
+
+            // Wait for some time to get input
+            interval.tick().await;
+
+            let stdin = stdin.lock();
+            let keys: Vec<Key> = stdin.keys().map(|key| key.unwrap()).collect();
+
+            if !keys.is_empty() {
+                let mut inputs = inputs.lock().unwrap();
+                inputs.extend(keys);
+
+                // Notify main thread
+                notify.notify();
+            }
+        }
+    });
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let stdin = io::stdin();
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     let mut run = true;
+
+    // FIFO of the inputs
+    let inputs = Arc::new(Mutex::new(VecDeque::<Key>::new()));
+    // Notifies the main thread when to redraw
+    let notify = Arc::new(Notify::new());
+
+    // Starts input thread
+    input_thread(&inputs, &notify);
+
     while run {
         terminal.draw(|f| {
             let size = f.size();
@@ -49,10 +87,11 @@ async fn main() -> io::Result<()> {
             f.render_widget(block, size);
         })?;
 
-        let handle = stdin.lock();
-        for key in handle.keys() {
-            let c = key.unwrap();
-            match c {
+        notify.notified().await;
+
+        let mut inputs = inputs.lock().unwrap();
+        for key in inputs.drain(..) {
+            match key {
                 Key::Char('q') => {
                     run = false;
                     break;
