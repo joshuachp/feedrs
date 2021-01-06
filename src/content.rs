44c11e45992a4d5edcbabcf9a596c503c416675b
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2021-01-06 11:24:27 +0100

    Added parsing functionality fro the content

--- src/content.rs
+++ src/content.rs
@@ -1,4 +1,5 @@
 use anyhow;
+use html2text::from_read;
 use sqlx::FromRow;
 use std::hash::{Hash, Hasher};
 use syndication::Feed;
@@ -25,26 +26,75 @@ pub fn parse_content(source: &str, content: String) -> anyhow::Result<Vec<Articl
         Feed::Atom(feed) => feed
             .entries()
             .iter()
-            .map(|entry| Article {
-                id: String::from(entry.id()),
-                source: String::from(source),
-                title: String::from(entry.title()),
-                sub_title: String::from(entry.summary().unwrap_or("")),
-                // TODO: Set content
-                content: String::from(""),
+            .map(|entry| {
+                let content = if let Some(content) = entry.content() {
+                    parse_html(content.value().unwrap_or(""))
+                } else {
+                    String::from("")
+                };
+                return Article {
+                    id: String::from(entry.id()),
+                    source: String::from(source),
+                    title: String::from(entry.title()),
+                    sub_title: parse_html(entry.summary().unwrap_or("")),
+                    content,
+                };
             })
             .collect(),
         Feed::RSS(channel) => channel
             .items()
             .iter()
-            .map(|item| Article {
-                id: String::from(""),
-                source: String::from(""),
-                title: String::from(item.title().unwrap_or("")),
-                sub_title: String::from(item.description().unwrap_or("")),
-                // TODO: Set content
-                content: String::from(""),
+            .map(|item| {
+                let content = if let Some(content) = item.content() {
+                    parse_html(content)
+                } else {
+                    String::from("")
+                };
+                return Article {
+                    id: String::from(""),
+                    source: String::from(""),
+                    title: String::from(item.title().unwrap_or("")),
+                    sub_title: parse_html(item.description().unwrap_or("")),
+                    content,
+                };
             })
             .collect(),
     })
+}
+
+/*
+ * Parse the html of a feed item content into simple text
+ */
+fn parse_html(content: &str) -> String {
+    // TODO: Set proper line length, default to 80
+    String::from(from_read(content.as_bytes(), 80).trim())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_html_base() {
+        let expected = String::from("Hello World!");
+        assert_eq!(parse_html("<span>Hello World!</span>"), expected);
+    }
+
+    #[test]
+    fn test_parse_html_multi() {
+        let expected = String::from("Hello World!\n\nGood Bye World!");
+        assert_eq!(
+            parse_html("<p>Hello World!</p><p>Good Bye World!</p>"),
+            expected
+        );
+    }
+
+    #[test]
+    fn test_parse_html_link() {
+        let expected = String::from("Here is a [link][1]\n\n[1] https://example.com");
+        assert_eq!(
+            parse_html("<span>Here is a <a href=\"https://example.com\">link</a></span>"),
+            expected
+        );
+    }
 }
